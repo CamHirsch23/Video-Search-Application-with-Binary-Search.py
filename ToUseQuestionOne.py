3c07118796429bce0a8e19b35661e23eb8739c41
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-04 11:55:08 -0500

    Create ToUseQuestionOne.py

---
+++ ToUseQuestionOne.py
@@ -0,0 +1,9 @@
+To use this code:
+
+Save it in a Python file, for example, app.py.
+
+Run the file in your terminal or command prompt with python app.py.
+
+The Flask server will start, and you can use a web browser or a tool like Postman to make GET requests to http://127.0.0.1:5000/search?title=Video%20Title to search for videos by title.
+
+Remember to replace Video%20Title with the title you are searching for, making sure to URL encode any spaces or special characters.
